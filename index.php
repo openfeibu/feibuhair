6cb4e1d9c4f5c6ac8754d16fb74f95490c4cd2b2
wzj <wzj@8371d1b3-9a36-704a-9887-b51531c70610>
2019-11-28 08:00:26 +0000

    git-svn-id: https://39.108.144.9:8443/svn/feibuhair@16 8371d1b3-9a36-704a-9887-b51531c70610

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ((DEBUG_MODE & 2) != 2)
 $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
 
 $uachar = "/(nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|mobile)/i";
-
+/*
 if(($ua == '' || preg_match($uachar, $ua))&& !strpos(strtolower($_SERVER['REQUEST_URI']),'wap'))
 {
     $Loaction = 'h5/';
@@ -35,7 +35,7 @@ if(($ua == '' || preg_match($uachar, $ua))&& !strpos(strtolower($_SERVER['REQUES
         exit;
     }
 
-}
+}*/
 /*------------------------------------------------------ */
 //-- Shopex系统地址转换
 /*------------------------------------------------------ */
