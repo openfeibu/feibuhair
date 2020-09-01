56e848b1774948271f1e218bbad88540c545af7d
wzj <wzj@8371d1b3-9a36-704a-9887-b51531c70610>
2020-09-01 09:56:42 +0000

    git-svn-id: https://39.108.144.9:8443/svn/feibuhair@48 8371d1b3-9a36-704a-9887-b51531c70610

--- article.php
+++ article.php
@@ -12,6 +12,8 @@
  * $Id: article.php 17217 2011-01-19 06:29:08Z liubo $
 */
 
+//ini_set('error_reporting',-1);  // 显示所有错误
+//ini_set('error_log','./error.log'); 
 define('IN_ECS', true);
 
 require(dirname(__FILE__) . '/includes/init.php');
@@ -128,40 +130,7 @@ else
     $smarty->display('article_pro.dwt', $cache_id);
 }
 
-/*------------------------------------------------------ */
-//-- PRIVATE FUNCTION
-/*------------------------------------------------------ */
 
-/**
- * 获得指定的文章的详细信息
- *
- * @access  private
- * @param   integer     $article_id
- * @return  array
- */
-function get_article_info($article_id)
-{
-    /* 获得文章的信息 */
-    $sql = "SELECT a.*, IFNULL(AVG(r.comment_rank), 0) AS comment_rank ".
-            "FROM " .$GLOBALS['ecs']->table('article'). " AS a ".
-            "LEFT JOIN " .$GLOBALS['ecs']->table('comment'). " AS r ON r.id_value = a.article_id AND comment_type = 1 ".
-            "WHERE a.is_open = 1 AND a.article_id = '$article_id' GROUP BY a.article_id";
-    $row = $GLOBALS['db']->getRow($sql);
-
-    if ($row !== false)
-    {
-        $row['comment_rank'] = ceil($row['comment_rank']);                              // 用户评论级别取整
-        $row['add_time']     = local_date($GLOBALS['_CFG']['date_format'], $row['add_time']); // 修正添加时间显示
-
-        /* 作者信息如果为空，则用网站名称替换 */
-        if (empty($row['author']) || $row['author'] == '_SHOPHELP')
-        {
-            $row['author'] = $GLOBALS['_CFG']['shop_name'];
-        }
-    }
-
-    return $row;
-}
 
 /**
  * 获得文章关联的商品
